70b1d12199f55769c56e0d4dd17db603d3994d6f
Eaeajj <eccentricmadm@gmail.com>
2021-11-22 14:53:07 +0300

    Add Header Disciplines

--- src/pages/student/index.tsx
+++ src/pages/student/index.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import Layout from "@components/common/layouts/Layout";
+import React, { ReactElement } from "react";
+import Disciplines from "../../components/student/disciplines/Disciplines";
+import styles from "../../styles/Student.module.css";
 
 const index = () => {
   return (
-    <div>
-      <div>student</div>
+    <div className={styles.page}>
+      <Disciplines />
     </div>
   );
 };
 
 export default index;
+
+index.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
